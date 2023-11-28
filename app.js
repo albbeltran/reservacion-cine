a67e92e4b93f32ab07e44f01259a79fb1565f0b6
Alberto Beltran <albertobeltran@protonmail.com>
2023-11-27 20:29:46 -0600

    improved styles and frontend logic

--- app.js
+++ app.js
@@ -16,13 +16,26 @@ app.use(express.urlencoded());       // for application/x-www-form-urlencoded
 // cors
 app.use(cors());
 
+// obtener todas las butacas
+app.get('/butaca', async (req, res) => {
+    try {
+        const result = await query('SELECT * FROM butacas;');
+        res.send(result);
+    } catch (err) {
+        console.log('Hubo un error', err);
+    }
+})
 
 // obtener estado de la butaca
 app.get('/butaca/:id', async (req, res) => {
     try {
         const id = req.params.id;
         const result = await query('SELECT estado FROM butacas WHERE id = ?', [id]);
-        res.send(result);
+        const estado = result[0].estado;
+
+        if (estado != 'on') res.sendStatus(200);
+        else res.sendStatus(400);
+
     } catch (err) {
         console.log('Hubo un error', err);
     }
@@ -33,16 +46,11 @@ app.post('/butaca/:id', async (req, res) => {
     try {
         const data = req.body;
         const id = req.params.id;
-        const result = await query('SELECT estado FROM butacas WHERE id = ?', [id]);
-        const estado = result[0].estado;
-        if(estado != 'on') {
-            await query('UPDATE butacas SET estado = ? WHERE id = ?', [data.estado, id]);
-            res.sendStatus(200);
-        } else res.sendStatus(400);
-
+        await query(`UPDATE butacas SET estado = ? WHERE id = ?`, [data.estado, id]);
+        res.sendStatus(200);
     } catch (err) {
         console.log('Hubo un error', err);
-        res.sendStatus(500);
+        res.sendStatus(400);
     }
 });
 
